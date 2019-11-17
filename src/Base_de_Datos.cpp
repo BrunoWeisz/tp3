7098a83d5f322ba0e6d8227d96f0b0266db64868
Alejandro <amercado@dc.uba.ar>
2019-11-17 16:08:57 -0300

    Modificaciones

--- src/Base_de_Datos.cpp
+++ src/Base_de_Datos.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "Base_de_Datos.h"
+
+//Como se escribia para devolver por referencia?
+linear_set<Registro> Base_de_Datos::consultar(Consulta &c){
+
+    //1 y 2)De complejidad
+    if(c.tipo_consulta()== SELECT && c.subconsulta1().tipo_consulta()==FROM){
+        string campo= c.campo1(); //O(|c|)
+        string nombreDeLaTabla= c.subconsulta1().nombre_tabla();//O(|t|)
+        string valor=c.valor(); //O(|v|)
+        Tabla tabla=tablas.at(nombreDeLaTabla);// O(|t|) porque devuelve por referencia, solo tiene que encontrar a la tabla que, en un trie es O(|t|)
+        linear_set<Registro> aDevolver; //O(1), creo el set de registros que voy a devolver.
+
+
+        //Caso select con clave:
+        if(campo==tabla.clave()){//O(|c|)
+            aDevolver.fast_insert(tabla[valor]);//O(1) el fast_insert y O(|v|) el tabla[valor] --> O(|v|)
+            return aDevolver; //O(1) porque devuelvo por referencia??
+            //Luego, si es select por clave en total es O(|c|+|v|+|t|)
+        }else{
+
+
+        }
+
+        //Caso select sin clave:
+
+    }
+}
--- src/Base_de_Datos.h
+++ src/Base_de_Datos.h
@@ -5,8 +5,22 @@
 #ifndef TP3_BASE_DE_DATOS_H
 #define TP3_BASE_DE_DATOS_H
 
+#include "../src/modulos_basicos/linear_set.h"
+#include "Tipos.h"
+#include "Registro.h"
+#include "../src/modulos_basicos/string_map.h"
+#include "Tabla.h"
+#include "Consulta.h"
 
 class Base_de_Datos {
+
+private:
+
+string_map<Tabla> tablas;
+
+public:
+
+linear_set<Registro> consultar(Consulta &c);
 
 };
 
