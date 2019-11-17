186c30d6cfeeff83ca50ef438000aa7ce65bd7e6
Alejandro <amercado@dc.uba.ar>
2019-11-17 15:14:53 -0300

    Agregue la base de datos

---
+++ src/Base_de_Datos.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alejandro on 11/17/2019.
+//
+
+#include "Base_de_Datos.h"
---
+++ src/Base_de_Datos.h
@@ -0,0 +1,14 @@
+//
+// Created by Alejandro on 11/17/2019.
+//
+
+#ifndef TP3_BASE_DE_DATOS_H
+#define TP3_BASE_DE_DATOS_H
+
+
+class Base_de_Datos {
+
+};
+
+
+#endif //TP3_BASE_DE_DATOS_H
